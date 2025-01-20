c952b61cb83a42ec26d9e4dbbfb587280d45fe1e
Hisham Bin Doheash <heshamdoheash@gmail.com>
2025-01-20 15:39:26 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,9 +4,9 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+  //Edit made by Hisham
 
-
-  //Last line print('Good bye')
+  //Last line print('TA TA')
 }
 
 void testMethod2()
